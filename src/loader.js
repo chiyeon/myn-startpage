e16148df1fed6d3b1d89d76010ec5e6edafdb4d3
Benjamin Wong <chiyeonwastaken@gmail.com>
2021-06-23 22:33:56 -0700

    version 1.1: no inline javascript; switched to nodes for creating html elements

--- src/loader.js
+++ src/loader.js
@@ -40,14 +40,14 @@ function build_page() {
 	for(var i = 0; i < num_pages; i++) {
 		// make the first page visible, all others hidden
 		var hidden = (i == 0) ? false : true;
-		body.innerHTML += generate_link_container(hidden);
+		body.appendChild(generate_link_container(hidden));
 	}
 
 	// for each page or 'container', populate corresponding links & titles
 	var containers = document.getElementsByClassName("links-container");
 	for(var i = 0; i < containers.length; i++) {
 		pages[i].forEach(link => {
-			containers[i].innerHTML += generate_link_element(link);	
+			containers[i].appendChild(generate_link_element(link));
 		});
 	}
 
@@ -56,21 +56,42 @@ function build_page() {
 	for(var i = 1; i <= num_pages; i++) {
 		// apply 'selected' css class to first nav button
 		var selected = (i == 1) ? true : false;
-		navigation_container.innerHTML += generate_navigation_button(i, selected);
+		navigation_container.appendChild(generate_navigation_button(i, selected));
 	}
 }
 
 // creates a link element to a specified url
 function generate_link_element(link_data) {
-	return `<a class="link" style="color: ${foreground_color}" href=${link_data.link}>${link_data.title}</a>`;
+	var link = document.createElement("a");
+	link.classList.add("link");
+	link.style.color = foreground_color;
+	link.href = link_data.link;
+	link.textContent = link_data.title;
+
+	return link;
 }
 
 // creates a container for each page
 function generate_link_container(is_hidden) {
-	return `<div class="links-container${is_hidden ? " hidden" : ""}"></div>`
+	var container = document.createElement("div");
+	container.classList.add("links-container");
+	if(is_hidden)
+		container.classList.add("hidden");
+
+	return container;
 }
 
 // creates a button to select a particular page
 function generate_navigation_button(number, is_selected) {
-	return `<button style="color: ${alt_foreground_color}" ${is_selected ? 'class="selected" ' : ''}onclick="select_page(${number - 1})">${number}</button>`
+
+	var button = document.createElement("button");
+	button.style.color = alt_foreground_color;
+	button.textContent = number;
+	button.addEventListener("click", () => {
+		select_page(number - 1);
+	})
+	if(is_selected)
+		button.classList.add("selected");
+
+	return button;
 }
